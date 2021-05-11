73eff27e2f7bec137c9e6d623f0d5a96ed8f5add
Mohammad <M.taba1508@gmail.com>
2021-05-11 17:42:20 +0430

    homework1

--- web/script.js
+++ web/script.js
@@ -1,21 +1,30 @@
-fetch("https://jsonplaceholder.typicode.com/users")
-  .then(res) => res.json()
-  .then((users) => {
-    prettyPrint(users)
+// fetch("https://jsonplaceholder.typicode.com/users")
+//   .then(res => res.json())
+//   .then(users => {
+//     prettyPrint(users)
 
-    let rawHtml = ''
+//     // console.log(`user's log:`, users);
 
-    users.map((user) => {
-      const keys = Object.keys(user)
+//     let rawHtml = ''
 
-      keys.map(key => rawHtml += `<p style="margin: 0.25em">${key}: <b style="color: #5BC236">${user[key]}</b></p>`)
-    })
+//     users.map((user) => {
+//       const keys = Object.keys(user)
+//       console.log(keys);
 
-    printToHtml(rawHtml)
-})
+
+//       keys.map(key => rawHtml += `<p style="margin: 0.25em">${key}: <b style="color: #5BC236">${user[key]}</b></p>`)
+//     })
+
+//     printToHtml(rawHtml)
+//   })
 
 //Mohammad's algorith for Jokes of the day
 
-// fetch("https://api.chucknorris.io/jokes/random")
-// .then((res)=>{res.json()})
-// .then((joke)=>{ChuckNorisJoke.innerHTML("joke")})
+fetch("https://api.chucknorris.io/jokes/random")
+  .then((res) => res.json())
+  .then((joke) => {
+    const chuckJoke = joke.value
+    callJoke(chuckJoke)
+    console.log(joke.value);
+  }
+  )
